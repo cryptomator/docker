78faf09f71d42cda334e5a485f51e6202750dafc
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2016-08-17 18:48:44 +0200

    removed settings path due to a bug in resolving ~

--- cryptomator-ppa/debian/cryptomator.sh
+++ cryptomator-ppa/debian/cryptomator.sh
@@ -3,5 +3,4 @@ java \
   -Xmx512m \
   -Dcryptomator.logPath=~/.Cryptomator/cryptomator.log \
   -Dcryptomator.upgradeLogPath=~/.Cryptomator/upgrade.log \
-  -Dcryptomator.settingsPath=~/.Cryptomator/settings.json \
   -jar /usr/share/java/cryptomator/Cryptomator-##CRYPTOMATOR_VERSION##.jar
