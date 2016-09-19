1916439c5c515372c55a2a2259cb85d7670bbe0f
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2016-09-19 22:25:09 +0200

    release 1.2.0

--- cryptomator-ppa/debian/cryptomator.sh
+++ cryptomator-ppa/debian/cryptomator.sh
@@ -3,4 +3,5 @@ java \
   -Xmx512m \
   -Dcryptomator.logPath=~/.Cryptomator/cryptomator.log \
   -Dcryptomator.upgradeLogPath=~/.Cryptomator/upgrade.log \
+  -Dcryptomator.settingsPath=~/.Cryptomator/settings.json \
   -jar /usr/share/java/cryptomator/Cryptomator-##CRYPTOMATOR_VERSION##.jar
