a37cb68b17a3e1593f04ce7c1a85cb1c514670a5
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2018-07-25 16:19:49 +0200

    updated ppa source build, now wrapping appimage

--- cryptomator-ppa/build.sh
+++ cryptomator-ppa/build.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-# download and extract sources
-curl -o cryptomator_${CRYPTOMATOR_VERSION}.orig.tar.gz -L https://github.com/cryptomator/cryptomator/releases/download/${CRYPTOMATOR_VERSION}/antkit.tar.gz
-mkdir cryptomator_${CRYPTOMATOR_VERSION}
-tar -xzf cryptomator_${CRYPTOMATOR_VERSION}.orig.tar.gz -C cryptomator_${CRYPTOMATOR_VERSION}
-cp -r /var/build/debian cryptomator_${CRYPTOMATOR_VERSION}/debian
-cd /var/build/cryptomator_${CRYPTOMATOR_VERSION}
+# download and prepare sources
+mkdir cryptomator_${PACKAGE_VERSION}
+curl -o cryptomator-${APPIMG_VERSION}-x86_64.AppImage -L https://dl.bintray.com/cryptomator/cryptomator/cryptomator-${APPIMG_VERSION}-x86_64.AppImage
+tar -czf cryptomator_${PACKAGE_VERSION}.orig.tar.gz cryptomator-${APPIMG_VERSION}-x86_64.AppImage
+cp cryptomator-${APPIMG_VERSION}-x86_64.AppImage cryptomator_${PACKAGE_VERSION}/
+cp -r debian cryptomator_${PACKAGE_VERSION}/debian
+pushd cryptomator_${PACKAGE_VERSION}
 
 # substitute variables
 RFC2822_TIMESTAMP=`date --rfc-2822`
-sed -i -e "s/##CRYPTOMATOR_VERSION##/${CRYPTOMATOR_VERSION}/g" debian/Cryptomator.desktop
-sed -i -e "s/##CRYPTOMATOR_VERSION##/${CRYPTOMATOR_VERSION}/g" debian/cryptomator.sh
-sed -i -e "s/##CRYPTOMATOR_FULL_VERSION##/${CRYPTOMATOR_FULL_VERSION}/g" debian/changelog
+sed -i -e "s/##PACKAGE_VERSION##/${PACKAGE_VERSION}/g" debian/Cryptomator.desktop
+sed -i -e "s/##PPA_VERSION##/${PPA_VERSION}/g" debian/changelog
 sed -i -e "s/##RFC2822_TIMESTAMP##/${RFC2822_TIMESTAMP}/g" debian/changelog
-sed -i -e "s/##CRYPTOMATOR_FULL_VERSION##/${CRYPTOMATOR_FULL_VERSION}/g" debian/files
+sed -i -e "s/##APPIMG_VERSION##/${APPIMG_VERSION}/g" debian/cryptomator.links
+sed -i -e "s/##APPIMG_VERSION##/${APPIMG_VERSION}/g" debian/source/include-binaries
 
 # build source package
-if [[ $CRYPTOMATOR_FULL_VERSION =~ .*ppa1$ ]]
+if [[ ${PPA_VERSION} =~ .*ppa1$ ]]
 then
   debuild -S -sa -uc -us
 else
@@ -24,24 +25,24 @@ else
 fi
 
 # sign .dsc file by hand (debsign would need a tty)
-cd /var/build
-DSC_SIZE_ORIG=`wc -c < cryptomator_${CRYPTOMATOR_FULL_VERSION}.dsc`
-DSC_MD5_ORIG=`md5sum cryptomator_${CRYPTOMATOR_FULL_VERSION}.dsc | cut -d' ' -f1`
-DSC_SHA1_ORIG=`sha1sum cryptomator_${CRYPTOMATOR_FULL_VERSION}.dsc | cut -d' ' -f1`
-DSC_SHA256_ORIG=`sha256sum cryptomator_${CRYPTOMATOR_FULL_VERSION}.dsc | cut -d' ' -f1`
-gpg --clearsign --no-tty --passphrase ${GPG_PASSPHRASE} --output cryptomator_${CRYPTOMATOR_FULL_VERSION}.dsc.gpg cryptomator_${CRYPTOMATOR_FULL_VERSION}.dsc
-mv cryptomator_${CRYPTOMATOR_FULL_VERSION}.dsc.gpg cryptomator_${CRYPTOMATOR_FULL_VERSION}.dsc
-DSC_SIZE_NEW=`wc -c < cryptomator_${CRYPTOMATOR_FULL_VERSION}.dsc | cut -d' ' -f1`
-DSC_MD5_NEW=`md5sum cryptomator_${CRYPTOMATOR_FULL_VERSION}.dsc | cut -d' ' -f1`
-DSC_SHA1_NEW=`sha1sum cryptomator_${CRYPTOMATOR_FULL_VERSION}.dsc | cut -d' ' -f1`
-DSC_SHA256_NEW=`sha256sum cryptomator_${CRYPTOMATOR_FULL_VERSION}.dsc | cut -d' ' -f1`
+popd
+DSC_SIZE_ORIG=`wc -c < cryptomator_${PPA_VERSION}.dsc`
+DSC_MD5_ORIG=`md5sum cryptomator_${PPA_VERSION}.dsc | cut -d' ' -f1`
+DSC_SHA1_ORIG=`sha1sum cryptomator_${PPA_VERSION}.dsc | cut -d' ' -f1`
+DSC_SHA256_ORIG=`sha256sum cryptomator_${PPA_VERSION}.dsc | cut -d' ' -f1`
+gpg --clearsign --no-tty --passphrase ${GPG_PASSPHRASE} --output cryptomator_${PPA_VERSION}.dsc.gpg cryptomator_${PPA_VERSION}.dsc
+mv cryptomator_${PPA_VERSION}.dsc.gpg cryptomator_${PPA_VERSION}.dsc
+DSC_SIZE_NEW=`wc -c < cryptomator_${PPA_VERSION}.dsc | cut -d' ' -f1`
+DSC_MD5_NEW=`md5sum cryptomator_${PPA_VERSION}.dsc | cut -d' ' -f1`
+DSC_SHA1_NEW=`sha1sum cryptomator_${PPA_VERSION}.dsc | cut -d' ' -f1`
+DSC_SHA256_NEW=`sha256sum cryptomator_${PPA_VERSION}.dsc | cut -d' ' -f1`
 
 # adjust and sign .changes file by hand
-sed -i -e "s/${DSC_MD5_ORIG} ${DSC_SIZE_ORIG}/${DSC_MD5_NEW} ${DSC_SIZE_NEW}/g" cryptomator_${CRYPTOMATOR_FULL_VERSION}_source.changes
-sed -i -e "s/${DSC_SHA1_ORIG} ${DSC_SIZE_ORIG}/${DSC_SHA1_NEW} ${DSC_SIZE_NEW}/g" cryptomator_${CRYPTOMATOR_FULL_VERSION}_source.changes
-sed -i -e "s/${DSC_SHA256_ORIG} ${DSC_SIZE_ORIG}/${DSC_SHA256_NEW} ${DSC_SIZE_NEW}/g" cryptomator_${CRYPTOMATOR_FULL_VERSION}_source.changes
-gpg --clearsign --no-tty --passphrase ${GPG_PASSPHRASE} --output cryptomator_${CRYPTOMATOR_FULL_VERSION}_source.changes.gpg cryptomator_${CRYPTOMATOR_FULL_VERSION}_source.changes
-mv cryptomator_${CRYPTOMATOR_FULL_VERSION}_source.changes.gpg cryptomator_${CRYPTOMATOR_FULL_VERSION}_source.changes
+sed -i -e "s/${DSC_MD5_ORIG} ${DSC_SIZE_ORIG}/${DSC_MD5_NEW} ${DSC_SIZE_NEW}/g" cryptomator_${PPA_VERSION}_source.changes
+sed -i -e "s/${DSC_SHA1_ORIG} ${DSC_SIZE_ORIG}/${DSC_SHA1_NEW} ${DSC_SIZE_NEW}/g" cryptomator_${PPA_VERSION}_source.changes
+sed -i -e "s/${DSC_SHA256_ORIG} ${DSC_SIZE_ORIG}/${DSC_SHA256_NEW} ${DSC_SIZE_NEW}/g" cryptomator_${PPA_VERSION}_source.changes
+gpg --clearsign --no-tty --passphrase ${GPG_PASSPHRASE} --output cryptomator_${PPA_VERSION}_source.changes.gpg cryptomator_${PPA_VERSION}_source.changes
+mv cryptomator_${PPA_VERSION}_source.changes.gpg cryptomator_${PPA_VERSION}_source.changes
 
 # upload
-dput ppa:sebastian-stenzel/cryptomator cryptomator_${CRYPTOMATOR_FULL_VERSION}_source.changes
+dput ppa:sebastian-stenzel/cryptomator cryptomator_${PPA_VERSION}_source.changes
--- cryptomator-ppa/debian/cryptomator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-java \
-  -Xmx512m \
-  -Dcryptomator.logPath=~/.Cryptomator/cryptomator.log \
-  -Dcryptomator.upgradeLogPath=~/.Cryptomator/upgrade.log \
-  -Dcryptomator.settingsPath=~/.Cryptomator/settings.json \
-  -jar /usr/share/java/cryptomator/Cryptomator-##CRYPTOMATOR_VERSION##.jar
